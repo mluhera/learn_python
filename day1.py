f991884e07b41e9d480c977b5eb792299c562ad4
mluhera <mluhera@noon.com>
2024-01-04 21:05:15 +0530

    Create day1.py
    Day 1 - For Loop

---
+++ day1.py
@@ -0,0 +1,31 @@
+# Loops
+
+# For loops iterate over a given sequence.
+
+# numbers = [2,3,4,3,4,5]
+# for i in numbers:
+#     print(i)
+
+
+
+# Break Statement - Stop the loop
+# sting = 'ManjitLuheraIsthebest'
+# for s in sting:
+#     if s == 'L':
+#         break
+#     print(s)
+
+# Continue Statement - skip the current block and return the next
+# sting = 'ManjitLuheraIsthebest'
+# for s in sting:
+#     if s == 'L':
+#         continue
+#     print(s)
+
+# Range function - return a sequence of numbers starting with 0 , ends -1
+
+for i in range(10):
+    print(i+1)
+
+
+        
