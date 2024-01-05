7c00a68216bc0b2e9892fe6c9e940a20acaf796b
mluhera <mluhera@noon.com>
2024-01-05 23:54:05 +0530

    Update day1.py
    update day1

--- day1.py
+++ day1.py
@@ -24,8 +24,52 @@
 
 # Range function - return a sequence of numbers starting with 0 , ends -1
 
-for i in range(10):
-    print(i+1)
+# for i in range(10):
+#     print(i+1)
+
+
+# ------------------------------------------------------------------------------------
+
+# While Loop - Execute a set of statement as long as condition is True
+
+# num1 = 0
+
+# while num1<10:
+#     print(f'Num {num1}')
+#     num1 = num1 + 1
+
+
+# --------------------------------------------------------------------------------------
+
+# Question practice
+
+# 1. Write a Python program that uses a for loop to find and print the sum of all even numbers from 1 to 50.
+
+# even_num = 0
+
+# for number in range(1,51):
+#     if number % 2 == 0:
+#         even_num = number + even_num
+
+# print("sum of even number from 1 to 50 is :",even_num)
+
+# 2. Factorial Calculation:
+#    Create a Python program that calculates and prints the factorial of a given number using a for loop. Take the input from the user.
+
+
+number = int(input("Enter your number to calculate its factorial"))
+
+if number < 1:
+    print("invalid Number")
+else: 
+    factorial_num = 1
+
+    for i in range(1,number + 1):
+        factorial_num = i * factorial_num
+
+print(factorial_num)
+
+
 
 
         
